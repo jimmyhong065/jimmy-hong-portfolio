51905883949e168c7c4bbf6dd0f832097eb7ea73
jimmyhong065 <jimmyhong@seekrtech.com>
2026-05-16 20:33:04 +0800

    feat: wire Blog, BlogPost, About routes in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import { HelmetProvider } from 'react-helmet-async'
 import Home from './pages/Home'
 import Projects from './pages/Projects'
 import ProjectDetail from './pages/ProjectDetail'
+import Blog from './pages/Blog'
+import BlogPost from './pages/BlogPost'
+import About from './pages/About'
 
 export default function App() {
   return (
@@ -12,9 +15,9 @@ export default function App() {
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
           <Route path="/projects/:id" element={<ProjectDetail />} />
-          <Route path="/blog" element={<div>Blog</div>} />
-          <Route path="/blog/:slug" element={<div>BlogPost</div>} />
-          <Route path="/about" element={<div>About</div>} />
+          <Route path="/blog" element={<Blog />} />
+          <Route path="/blog/:slug" element={<BlogPost />} />
+          <Route path="/about" element={<About />} />
           <Route path="/login" element={<div>Login</div>} />
           <Route path="/admin/*" element={<div>Admin</div>} />
         </Routes>
