7bad771d72edff3ada42bd6d7f19b37fb7b767b2
jimmyhong065 <jimmyhong@seekrtech.com>
2026-05-30 09:37:15 +0800

    test: add jsdom polyfills for tiptap/prosemirror

--- src/test-setup.js
+++ src/test-setup.js
@@ -1,1 +1,26 @@
 import '@testing-library/jest-dom'
+
+// ProseMirror / TipTap requires these browser APIs not fully in jsdom
+if (!global.document.createRange) {
+  global.document.createRange = () => ({
+    setStart: () => {},
+    setEnd: () => {},
+    commonAncestorContainer: {
+      nodeName: 'BODY',
+      ownerDocument: document,
+    },
+    getBoundingClientRect: () => ({ top: 0, left: 0, bottom: 0, right: 0, width: 0, height: 0 }),
+    getClientRects: () => [],
+  })
+}
+
+if (!global.window.getSelection) {
+  global.window.getSelection = () => ({
+    addRange: () => {},
+    removeAllRanges: () => {},
+    rangeCount: 0,
+    getRangeAt: () => null,
+  })
+}
+
+Element.prototype.scrollIntoView = () => {}
